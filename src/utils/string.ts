0a6266e5014d249b31d1a90bcfc5fe757662ecb7
Gustavo Daniel <gudaniel1@gmail.com>
2025-07-31 10:15:19 -0300

    Update utils/string.capitalize to return the Union types of strings

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,10 @@
-export const capitalize = (string: string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+type CapitalizeFn = {
+    <T extends string>(str: T): Capitalize<T>;
+    (str: string): string;
 };
+
+// The 'as' assertion is required to satisfy the function overload type.
+// This is a known limitation when implementing overloads with arrow functions.
+export const capitalize = ((string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}) as CapitalizeFn;
